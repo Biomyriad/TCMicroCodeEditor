7747dec6c05134e7b6af849d1b8a35050a4a3b11
Biomyriad <biomyriad@gmail.com>
2024-06-23 09:31:40 -0700

    fleshed out data structures to hold main data.

---
+++ main.js
@@ -0,0 +1,26 @@
+//localStorage.setItem("lastname", "Smith")
+//localStorage.getItem("lastname")
+//localStorage.removeItem("lastname")
+//localStorage.clear()
+
+
+const opCodeList = []
+const ctrlLineLut = []
+
+ctrlLineLut[0] = 'ip_to_addy'
+
+const opCodeEntry = (code,mnemonic) => {
+  this.code = code
+  this.mnemonic = mnemonic
+  this.mCycles = [] // 1 item per state
+}
+
+const myOp = new opCodeEntry('00', 'nop')
+mythis.mCycles.push(
+    // array of bits or ctrl lines
+    [653,43,1967,44,1,55] // needs to be as long as # ctrl lines
+  )
+
+opCodeList.push(myOp)
+
+
